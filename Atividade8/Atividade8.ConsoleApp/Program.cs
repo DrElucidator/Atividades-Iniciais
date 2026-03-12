ac7834bc6c8d26f88f4371aef5268520ff6722f2
Alec Luí <dr.elucidator@gmail.com>
2026-03-12 18:31:34 -0300

    Atividade 8 adicionada

---
+++ Atividade8/Atividade8.ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Verificador de números primos");
+Console.Write("Digite um número: ");
+int num = Convert.ToInt32(Console.ReadLine());
+bool primo = true;
+if (num < 2)
+{
+    primo = false;
+}
+else
+{
+    for (int i = 2; i <= num - 1; i++)
+    {
+        if (num % i == 0)
+        {
+            primo = false;
+            break;
+        }
+    }
+}
+if (primo)
+{
+    Console.WriteLine($"{num} é um número primo.");
+}
+else
+{
+    Console.WriteLine($"{num} não é um número primo.");
+}
