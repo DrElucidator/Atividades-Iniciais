ff79968bbd3e239190de24437519a2b88c70c3ec
Alec Luí <dr.elucidator@gmail.com>
2026-03-12 16:10:32 -0300

    Criadas atividades 1 , 2 e 3

---
+++ Atividade1/Atividade1.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Calculadora de Volume de Caixa Retangular");
+Console.WriteLine("Digite o comprimento da caixa:");
+decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Digite a largura da caixa:");
+decimal largura = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Digite a altura da caixa:");
+decimal altura = Convert.ToDecimal(Console.ReadLine());
+decimal volume = comprimento * largura * altura;
+Console.WriteLine($"O volume da caixa retangular é: {volume} cm³");
+Console.ReadLine();
