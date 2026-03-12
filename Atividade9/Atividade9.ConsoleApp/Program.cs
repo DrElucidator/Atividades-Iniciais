7e5950806eeaac03cc29e6a125de45f373a0703a
Alec Luí <dr.elucidator@gmail.com>
2026-03-12 18:50:45 -0300

    Atividade 9 adicionada

---
+++ Atividade9/Atividade9.ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Leitor de dimensões de terreno");
+Console.WriteLine("Digite a largura do terreno:");
+decimal largura = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Digite o comprimento do terreno:");
+decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+decimal area = largura * comprimento;
+Console.WriteLine($"A área do terreno é: {area} metros quadrados.");
