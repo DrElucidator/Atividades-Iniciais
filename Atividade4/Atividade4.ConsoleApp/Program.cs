b190efc9bc5ad025e162009dc384d4218d91bd22
Alec Luí <dr.elucidator@gmail.com>
2026-03-12 16:32:37 -0300

    Atividade 4 adicionada

---
+++ Atividade4/Atividade4.ConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Conversor de Temperatura - Celsius para Fahrenheit");
+Console.WriteLine("Digite a temperatura em Celsius:");
+decimal celsius = Convert.ToDecimal(Console.ReadLine());
+decimal fahrenheit = (celsius * 9 / 5) + 32;
+Console.WriteLine($"A temperatura em Fahrenheit é: {fahrenheit}");
