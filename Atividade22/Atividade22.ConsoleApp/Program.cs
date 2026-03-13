7b71c8e74c18c7c23319d301c44c572400760944
Alec Luí <dr.elucidator@gmail.com>
2026-03-13 17:18:49 -0300

    Atividade 22 adicionada

---
+++ Atividade22/Atividade22.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Escreva um número para gerar a tabela Fibonacci até ele: ");
+int limite = Convert.ToInt32(Console.ReadLine());
+int x = 0; int y = 1;
+while(x <= limite)
+{
+    Console.Write(x + " ");
+    int z = x;
+    x = y;
+    y = z + y;
+}
