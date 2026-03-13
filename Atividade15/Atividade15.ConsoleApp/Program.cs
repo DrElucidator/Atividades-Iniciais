57e02114688c0a8dc1bb2dcc2c4ccd4d823ddb4a
Alec Luí <dr.elucidator@gmail.com>
2026-03-13 13:18:58 -0300

    Atividade 15 adicionada

---
+++ Atividade15/Atividade15.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Calculador de IMC - Indice de Massa Corporal para adultos");
+Console.WriteLine("Digite sua altura: ");
+decimal altura = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Digite seu peso: ");
+decimal peso = Convert.ToDecimal(Console.ReadLine());
+decimal IMCAdult = peso / (altura * altura);
+if (IMCAdult < 18.5m)
+Console.WriteLine("Abaixo do peso ideal");
+else if (IMCAdult <= 25m)
+Console.WriteLine("Peso saudável");
+else
+Console.WriteLine("Obeso");
