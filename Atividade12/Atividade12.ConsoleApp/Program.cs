3fefec324d823021cb9a9a40c59e0f6e01788577
Alec Luí <dr.elucidator@gmail.com>
2026-03-13 12:06:27 -0300

    Atividade 12 adicionada

---
+++ Atividade12/Atividade12.ConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Calculadora de aumento de salário");
+Console.WriteLine("Digite o valor do salário atual: ");
+decimal salarioBase = Convert.ToDecimal(Console.ReadLine());
+decimal aumento = 0.15m; decimal descontos = 0.08m; decimal aumentoSalarial = salarioBase * aumento; decimal salarioAtual = aumentoSalarial + salarioBase; decimal descontoBase = salarioAtual * descontos; decimal novoSalario = salarioAtual - descontoBase;
+Console.WriteLine("O salário atualizado do funcionário é: " + novoSalario.ToString("C"));
