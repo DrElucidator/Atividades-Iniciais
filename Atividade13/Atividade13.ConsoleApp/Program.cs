b4812ad8d2da375bcc64fd9bc0b803f3ccab0a28
Alec Luí <dr.elucidator@gmail.com>
2026-03-13 12:23:11 -0300

    Atividade 13 adicionada

---
+++ Atividade13/Atividade13.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Comparador da soma de valores perante valor C");
+Console.WriteLine("Digite o valor A a ser somado: ");
+decimal valorA = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Digite o valor B a ser somado: ");
+decimal valorB = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Digite o valor C a ser comparado: ");
+decimal valorC = Convert.ToDecimal(Console.ReadLine());
+if (valorA + valorB < valorC)
+    Console.WriteLine("A Soma dos valores A & B são menores que C");
+else
+    Console.WriteLine("Valor de C é menor que a soma de A & B");
