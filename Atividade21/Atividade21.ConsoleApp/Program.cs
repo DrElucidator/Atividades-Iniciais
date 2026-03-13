c770ff5dc0ce38512effb4c1113fe0c78eb12abc
Alec Luí <dr.elucidator@gmail.com>
2026-03-13 16:46:19 -0300

    Atividade 21 adicionada

---
+++ Atividade21/Atividade21.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Digite um número para fazer o cálculo fatorial: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int fatorial = 1;
+Console.Write($"{A}! = ");
+for(int a = A ; a >= 1; a--)
+{
+    fatorial *= a;
+    if (a > 1)
+        Console.Write($"{a} x ");
+    else
+        Console.Write($"{a} ");
+}
+Console.WriteLine($"= {fatorial}");
