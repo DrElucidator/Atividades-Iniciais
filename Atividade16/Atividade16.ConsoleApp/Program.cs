1301f528e889672a2fbaf0d1772e0f3c549aa692
Alec Luí <dr.elucidator@gmail.com>
2026-03-13 15:01:52 -0300

    Atividade 16 adicionada

---
+++ Atividade16/Atividade16.ConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Digite algum número para verificar a paridade: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num % 2 == 0)
+    Console.WriteLine("PAR");
+else
+Console.WriteLine("IMPAR");
