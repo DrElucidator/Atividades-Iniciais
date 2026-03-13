0534f1f18b8824ac64e3960fb7f3a18a89a67881
Alec Luí <dr.elucidator@gmail.com>
2026-03-13 15:44:52 -0300

    Atividade 17 adicionada

---
+++ Atividade17/Atividade17.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Calculadora de soma (A+B) e divisão (A/B)");
+Console.WriteLine("Atribua um valor ao número A: ");
+decimal valorA = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Atribua um valor ao número B: ");
+decimal valorB = Convert.ToDecimal(Console.ReadLine());
+decimal valorC;
+if(valorA == valorB)
+valorC = valorA + valorB;
+else
+valorC = valorA * valorB;
+Console.WriteLine("O resultado do número C é: " + valorC);
