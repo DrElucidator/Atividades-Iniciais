84fb4271bb2e9282ddedab8e7cc76c276a3b887d
Alec Luí <dr.elucidator@gmail.com>
2026-03-13 11:28:10 -0300

    Atividade 11 adicionada

---
+++ Atividade11/Atividade11.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Contador de dias vividos");
+Console.WriteLine("Digite o seu nome: ");
+string? nome = Console.ReadLine();
+DateTime agora = DateTime.Now;
+Console.WriteLine("Digite a data de nascimento no formato [dd/MM/aaaa]: ");
+DateTime nascimento = Convert.ToDateTime((Console.ReadLine()));
+TimeSpan diasVividos = agora - nascimento;
+Console.WriteLine($"{nome}  nascido(a) em:  {nascimento.ToString("dd/MM/yyyy")}");
+if (nascimento != null)
+{
+    Console.WriteLine($"{nome}  viveu  {diasVividos.Days} dias.");
+}
+else
+{
+    Console.WriteLine("Digite uma data válida.");
+    return;
+}
+Console.ReadLine();
