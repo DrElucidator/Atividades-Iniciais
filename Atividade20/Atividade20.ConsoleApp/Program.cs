d3178659f6ec380451f7c3db4528b66766ab9027
Alec Luí <dr.elucidator@gmail.com>
2026-03-13 16:23:54 -0300

    Atividade 20 adicionada

---
+++ Atividade20/Atividade20.ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Digite um número para gerar a tabuada: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int N = 0; N <= 10; N++)
+{
+    int resultadoTabuada = n * N;
+    string operacaoTabuada = N + " x " + n + " = " + resultadoTabuada;
+    Console.WriteLine(operacaoTabuada);
+}
